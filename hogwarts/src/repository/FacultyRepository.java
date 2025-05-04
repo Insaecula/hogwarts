890070d3c0647cb9837e56da12edf39ec5ec5102
insaecula <morozzzik228@gmail.com>
2025-05-04 19:09:58 +0300

    homework

--- hogwarts/src/repository/FacultyRepository.java
+++ hogwarts/src/repository/FacultyRepository.java
@@ -4,4 +4,5 @@ import com.example.hogwarts.model.Faculty;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FacultyRepository extends JpaRepository<Faculty, Long> {
+    List<Faculty> findByColorIgnoreCase(String color);
 }
--- hogwarts/src/repository/StudentRepository.java
+++ hogwarts/src/repository/StudentRepository.java
@@ -5,4 +5,5 @@ import com.example.hogwarts.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    List<Student> findByAge(int age);
 }
