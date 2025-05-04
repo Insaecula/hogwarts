8f66d5dd16bb9a7e35c6b7b5dc2adfd3654d378d
insaecula <morozzzik228@gmail.com>
2025-05-04 22:17:20 +0300

    "Добавлены интеграционные тесты с использованием TestRestTemplate"

--- hogwarts/src/model/Student.java
+++ hogwarts/src/model/Student.java
@@ -12,6 +12,7 @@ public class Student {
     private Long id;
     private String name;
     private int age;
+
     public Long getId() {
         return id;
     }
@@ -35,6 +36,7 @@ public class Student {
     public void setAge(int age) {
         this.age = age;
     }
+
     @ManyToOne
     @JoinColumn(name = "faculty_id")
     private Faculty faculty
